cd02ef3546c44c40394ebfa3fb53aebc85e0efde
Weina Dai <77016782+Octobserver@users.noreply.github.com>
2024-01-10 04:07:55 +0000

    Customer Tab

--- Hello.py
+++ Hello.py
@@ -68,7 +68,42 @@ def run():
 
     with tab2:
       st.header("Customers")
-      st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+      # Customer Segmentation: A pie chart showing segmentation of customers based on their purchase behavior
+      # TODO: use UMAP for dimensional reduction and HDBSCAN for clustering
+      st.write("### Customer Segmentation")
+
+      # Customer Lifetime Value (CLV): A bar chart showing the CLV of different customer segments
+      st.write("### Customer Lifetime Value")
+
+      #Top Customers: A list or bar chart of top customers by sales
+      st.write("### Top Customers")
+      top_customers = conn.query('SELECT Customers.CustomerID AS CustomerName, SUM(Quantity * UnitPrice) AS TotalOrder FROM Customers INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID INNER JOIN [Order Details] ON Orders.OrderID = [Order Details].OrderID GROUP BY CustomerName ORDER BY TotalOrder DESC LIMIT 10')
+      st.bar_chart(top_customers, x = "CustomerName", y = "TotalOrder")
+
+      #Customer Geographic Distribution: A map showing where customers are located, which can be filtered by the date range and category.
+      st.write("### Customer Geographic Distribution")
+      geographic_distribution = conn.query('SELECT Country, COUNT(*) AS NumCustomers FROM Customers GROUP BY Country')
+      st.bar_chart(geographic_distribution, x = "Country", y = "NumCustomers")
+      country_codes = cc.pandas_convert(series=geographic_distribution.Country, to='ISO3')  
+      z = geographic_distribution.NumCustomers
+
+      layout = dict(geo={'scope': "world"})
+      data = dict(
+          type='choropleth',
+          locations=country_codes,
+          locationmode='ISO-3',
+          colorscale='Viridis',
+          z=z)
+      map = go.Figure(data=[data], layout=layout)
+      map.update_geos(projection_type="orthographic")
+      map.update_layout(height=300, margin={"r":0,"t":0,"l":0,"b":0})
+      st.plotly_chart(map)
+
+      #Orders by Customer: A bar chart showing the number of orders by customer
+      st.write("Orders by Customer")
+      orders_by_customer = conn.query('SELECT Customers.CustomerID AS CustomerName, COUNT(*) AS TotalOrder FROM Customers INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID GROUP BY Customers.CustomerID ORDER BY TotalOrder DESC')
+      st.bar_chart(orders_by_customer, x="CustomerName", y="TotalOrder")
+
 
     with tab3:
       st.header("Suppliers")
